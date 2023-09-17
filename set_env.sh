fed9fce42c5f1904b130987255123910e514d810
Your Name <you@example.com>
2023-09-16 21:25:59 -0700

    first commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=master
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=cdr.cyapw1mcmmdj.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::tungnd-project3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
