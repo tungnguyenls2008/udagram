71be4e88f1417da77d8aea71b86e71d5234607ab
Your Name <you@example.com>
2023-09-17 05:52:07 -0700

    update kubectl setting

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -6,12 +6,13 @@ export const sequelize = new Sequelize({
   'username': config.username,
   'password': config.password,
   'database': config.database,
-  'host': config.host,
+  // 'host': config.host,
+  'host': 'cdr.cyapw1mcmmdj.us-east-1.rds.amazonaws.com',
 
   'dialect': config.dialect,
   // 'dialectOptions': {
   //   'ssl': {
-  //     'require': true, // This will help you. But you will see nwe error
+  //     'require': true, // This will help. But you will see new error
   //     'rejectUnauthorized': false // This line will fix new error
   //   }
   // },
