5849bc07bcd1e792611485d8fa55237f9aa99a78
Your Name <you@example.com>
2023-09-20 12:04:51 +0700

    update

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -6,8 +6,8 @@ export const sequelize = new Sequelize({
   'username': config.username,
   'password': config.password,
   'database': config.database,
-  // 'host': config.host,
-  'host': 'cdr.cyapw1mcmmdj.us-east-1.rds.amazonaws.com',
+  'host': config.host,
+  //'host': 'cdr.cyapw1mcmmdj.us-east-1.rds.amazonaws.com',
 
   'dialect': config.dialect,
   // 'dialectOptions': {
