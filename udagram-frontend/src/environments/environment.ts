38b33f61a0e91dc90eb14c221ee47a6fb46054ca
overlord <tungnguyenls2008@gmail.com>
2023-10-05 15:37:11 +0700

    deploying

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://abf044a773ed949aa8c0ffd615e6f6d5-830708566.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
